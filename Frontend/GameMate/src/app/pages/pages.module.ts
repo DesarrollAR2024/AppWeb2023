8a7cf185e97e8e563b1f7f73ac76dec354444270
Jesica Ortiz <jesicaortiz15@outlook.com>
2023-04-29 21:07:56 -0300

    arreglo conflictos

--- Frontend/GameMate/src/app/pages/pages.module.ts
+++ Frontend/GameMate/src/app/pages/pages.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { TiendaComponent } from './pages/tienda/tienda.component';
+import { TiendaComponent } from './tienda/tienda.component';
 
 
 
