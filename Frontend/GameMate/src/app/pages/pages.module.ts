b2c07c31ba15a4acd3e6be0beee9616fe0bb6fba
Jesica Ortiz <jesicaortiz15@outlook.com>
2023-05-02 06:36:55 -0300

    cambios

--- Frontend/GameMate/src/app/pages/pages.module.ts
+++ Frontend/GameMate/src/app/pages/pages.module.ts
@@ -6,14 +6,16 @@ import { RegistroComponent } from './registro/registro.component';
 import { PagesRoutingModule } from './pages-routing.module';
 import { TerminosycondicinonesComponent } from './terminosycondicinones/terminosycondicinones.component';
 import { TiendaComponent } from './tienda/tienda.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { ProductoComponent } from './producto/producto.component';
 
 
 @NgModule({
-  declarations: [DashboardIndexComponent, LoginComponent, RegistroComponent, TerminosycondicinonesComponent, TiendaComponent],
+  declarations: [DashboardIndexComponent, LoginComponent, RegistroComponent, TerminosycondicinonesComponent, TiendaComponent, ContactoComponent, ProductoComponent],
   imports: [
     CommonModule,
     PagesRoutingModule
   ],
-  exports: [DashboardIndexComponent, LoginComponent, RegistroComponent, TerminosycondicionesComponent, ContactoComponent, ProductoComponent, TiendaComponent]
+  exports: [DashboardIndexComponent, LoginComponent, RegistroComponent, TerminosycondicinonesComponent, ContactoComponent, ProductoComponent, TiendaComponent]
 })
 export class PagesModule { }
