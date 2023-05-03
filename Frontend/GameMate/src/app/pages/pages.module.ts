ba20b2fded1a962be979f30278e2ea7307ce11e8
Triana Devia <106435543+TrianaDevia@users.noreply.github.com>
2023-05-03 08:06:34 -0300

    Revert "Develop"

--- Frontend/GameMate/src/app/pages/pages.module.ts
+++ Frontend/GameMate/src/app/pages/pages.module.ts
@@ -1,4 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TiendaComponent } from './tienda/tienda.component';
+import { DashboardIndexComponent } from './dashboard-index/dashboard-index.component';
+import { RegistroComponent } from './registro/registro.component';
+import { LoginComponent } from './login/login.component';
+import { AppRoutingModule } from 'app/app-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    TiendaComponent, RegistroComponent, LoginComponent, DashboardIndexComponent
+  ],
+  imports: [
+    CommonModule, AppRoutingModule
+  ],
+  exports: [DashboardIndexComponent, TiendaComponent, RegistroComponent, LoginComponent]
 })
 export class PagesModule { }
