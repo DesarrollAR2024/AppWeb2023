f61c5a26dc90b0f25f3ea112691d3336b53a731f
Triana Devia <trianadevia@gmail.com>
2023-05-05 16:40:08 -0300

    Arreglo conflictos

--- Frontend/GameMate/src/app/app.component.spec.ts
+++ Frontend/GameMate/src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import './nosotros.component.css';
+/*import './nosotros.component.css';*/
 
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
