f56dd6bba7028182401bce92154e834d572ceef7
EduRePiN <96556245+EduRePiN@users.noreply.github.com>
2023-05-28 17:39:38 -0300

    inyectado el formsModule y aplicado en login

--- Frontend/GameMate/src/app/pages/login/login.component.ts
+++ Frontend/GameMate/src/app/pages/login/login.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){
+    this.form= this.formBuilder.group({
+      mail:['',[]]
+      password:['',[]],
+    })
+  }
+
+  ngOnInit() {}
 
 }
