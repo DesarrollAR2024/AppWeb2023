8bbf2f892366585fcaa9057ff439a3ebcdddf455
EduRePiN <96556245+EduRePiN@users.noreply.github.com>
2023-05-28 18:49:14 -0300

    agregue validaciones al formulario de login

--- Frontend/GameMate/src/app/pages/login/login.component.ts
+++ Frontend/GameMate/src/app/pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -11,11 +11,28 @@ export class LoginComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder){
     this.form= this.formBuilder.group({
-      mail:['',[]]
-      password:['',[]],
+      mail:['',[Validators.required, Validators.email]],
+      password:['',[Validators.required, Validators.minLength(8)]]
     })
+  }
+  
+  get Password(){
+    return this.form.get("password");
+  }
+  
+  get Mail(){
+    return this.form.get("mail");
+  }
+  
+  onEnviar(event: Event){
+    event.preventDefault;
+    if (this.form.valid){
+      alert("Enviar al servidor...")
+    } else {
+      this.form.markAllAsTouched();
+    }
   }
 
   ngOnInit() {}
-
+  
 }
