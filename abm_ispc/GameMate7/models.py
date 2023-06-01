1625bd18767452a14c44fbfb1adca5490efa0155
AndreaGomezIT <andrugo1983@hotmail.com>
2023-06-01 07:15:31 -0300

    entorno virtual, cors, endpoints /registro y /login configurados

--- abm_ispc/GameMate7/models.py
+++ abm_ispc/GameMate7/models.py
@@ -1,7 +1,33 @@
-from django.db import models
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, Group, Permission
 from django.db.models.signals import post_save
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+
+
+
+
+#from django.utils import timezone
+#from django.db import models
+#from django.contrib.auth.models import User 
+#from django.db.models.signals import post_save
 # Create your models here.
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    groups = models.ManyToManyField(Group, related_name='customuser_set', blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name='customuser_set', blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
 
 class Categoria(models.Model):
     id_categoria= models.IntegerField(primary_key=True)
