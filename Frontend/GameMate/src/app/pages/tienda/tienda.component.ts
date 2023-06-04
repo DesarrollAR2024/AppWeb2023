5b33c9f55bb8fa8715cfb3ef858f1fdc1e625a3f
matiasp23 <matiaspedraza023@gmail.com>
2023-06-04 17:47:14 -0300

    cambios de la rama main nuevo usermodel

---
+++ Frontend/GameMate/src/app/pages/tienda/tienda.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { TiendaServiceService } from 'app/service/tienda-service.service';
+import { Tienda } from 'app/Tienda';
+
+@Component({
+  selector: 'app-tienda',
+  templateUrl: './tienda.component.html',
+  styleUrls: ['./tienda.component.css']
+})
+export class TiendaComponent {
+  tienda: Tienda[] = [];
+
+  constructor(private tiendaS: TiendaServiceService) {}
+
+  ngOnInit(): void{
+    this.getTienda();
+  }
+
+  getTienda(): void{
+    this.tiendaS.getTienda().subscribe(
+      data => {
+        this.tienda = data;
+      }
+    )
+  }
+}
