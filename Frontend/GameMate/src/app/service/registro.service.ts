b9ac196170adadff740251bd7593e6add0765f9e
Marcelo Rojas <108030104+PsicoDevil@users.noreply.github.com>
2023-06-04 21:03:42 -0300

    Se agregan las peticiones al servicio de registro

--- Frontend/GameMate/src/app/service/registro.service.ts
+++ Frontend/GameMate/src/app/service/registro.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Registro } from '../Registro';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
@@ -7,6 +6,27 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class RegistroService {
+  private apiUrl = 'http://localhost:8000/api/v1productos/'; // Reemplazar con la URL DE LA API
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getDatos(): Observable<any> {
+    return this.http.get<any>(this.apiUrl);
+  }
+
+  crearRegistro(data: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, data);
+  }
+
+  actualizarRegistro(id: string, data: any): Observable<any> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.put<any>(url, data);
+  }
+
+  eliminarRegistro(id: string): Observable<any> {
+    const url = `${this.apiUrl}/${id}`;
+    return this.http.delete<any>(url);
+  }
 }
+
+
