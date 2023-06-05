3d7b980fd83f41110ad609a28de48e18a2fac076
matiasp23 <matiaspedraza023@gmail.com>
2023-06-04 23:58:17 -0300

    forzando merge matiasp main-nuevo-usermodel

--- abm_ispc/GameMate7/models.py
+++ abm_ispc/GameMate7/models.py
@@ -1,6 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
+
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField(
+        max_length=150, unique=True
+    )
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    def __str__(self):
+        return self.email
+        
 
 class Categoria(models.Model):
     id_categoria= models.IntegerField(primary_key=True)
--- abm_ispc/GameMate7/views.py
+++ abm_ispc/GameMate7/views.py
@@ -3,6 +3,13 @@ from rest_framework import viewsets
 from .serializer import UsuariosSerializer, ProductoSerializer
 from .models import Usuarios, Producto
 from rest_framework.permissions import IsAdminUser, AllowAny, IsAuthenticated
+
+#Se agrega para gestionar vista de login y logout
+from django.contrib.auth import authenticate, login, logout
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 # Create your views here.
 
 class UsuariosViewSet(viewsets.ModelViewSet):
@@ -13,3 +20,23 @@ class verProductos(viewsets.ModelViewSet):
     permission_classes = [AllowAny] 
     queryset = Producto.objects.all()
     serializer_class = ProductoSerializer
+
+# Se agregan las clase de login y logount
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data.get('email',None)
+        pasword = request.data.get('password', None)
+        user = authenticate (email=email, pasword=pasword)
+
+        if user:
+            login(request, user)
+            return Response(status=status.HTTP_200_OK)
+         
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+class LogoutView(APIView):
+    def post(self, request):
+        logout(request)
+
+        return Response(status=status.HTTP_200_OK)
