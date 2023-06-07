7145f9b056f1e38c670511a727951b5fa9c0101b
matiasp23 <matiaspedraza023@gmail.com>
2023-06-07 02:07:04 -0300

    corrijo error url user/profile, serializer y formateo urlpatterns

--- abm_ispc/GameMate7/urls.py
+++ abm_ispc/GameMate7/urls.py
@@ -1,33 +1,22 @@
 from django.urls import path, include
 from rest_framework import routers
-#from Usuarios.views import UsuariosViewSet
 from GameMate7 import views
-#Se configura url de la app
-from.views import LoginView, LogoutView, SignupView, ProfileView, agregarProducto, ListarUsuarios 
+from .views import LoginView, LogoutView, SignupView, ProfileView, agregarProducto, ListarUsuarios
 
-router= routers.DefaultRouter()
-router.register(r'usuarios',views.UsuariosViewSet)
-router.register(r'productos',views.verProductos)
-router.register(r'categorias',views.verCategorias)
-router.register(r'proveedores',views.verProveedores)
-router.register(r'facturaciones',views.FacturacionViewSet)
-#----
+router = routers.DefaultRouter()
+router.register(r'usuarios', views.UsuariosViewSet)
+router.register(r'productos', views.verProductos)
+router.register(r'categorias', views.verCategorias)
+router.register(r'proveedores', views.verProveedores)
+router.register(r'facturaciones', views.FacturacionViewSet)
+
 urlpatterns = [
-     path('', include(router.urls)),
-       path('auth/login/',
-         LoginView.as_view(), name='auth_login'),
-
-    path('auth/logout/',
-         LogoutView.as_view(), name='auth_logout'),
-     path('auth/reset/',
-         include('django_rest_passwordreset.urls',
-                 namespace='password_reset')),
-     path('auth/registro/',
-         SignupView.as_view(), name='auth_signup'),
-     path('user/profile/',
-         ProfileView.as_view(), name='user_profile'),
-     path('usuarios/',
-         ListarUsuarios.as_view(), name='listar_usuarios'),
-     path('agregarproducto/',
-         agregarProducto.as_view(), name='agregar_producto'),
+    path('', include(router.urls)),
+    path('auth/login/', LoginView.as_view(), name='auth_login'),
+    path('auth/logout/', LogoutView.as_view(), name='auth_logout'),
+    path('auth/reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
+    path('auth/registro/', SignupView.as_view(), name='auth_signup'),
+    path('user/profile/', ProfileView.as_view(), name='user_profile'),
+    path('usuarios/', ListarUsuarios.as_view(), name='listar_usuarios'),
+    path('agregarproducto/', agregarProducto.as_view(), name='agregar_producto'),
 ]
