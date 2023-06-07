cd7d4f88189f6c6cf24e9b3de060f60096962771
Jesica Ortiz <jesicaortiz15@outlook.com>
2023-06-07 17:36:31 -0300

    agrego peticiones para tienda

--- Frontend/GameMate/src/app/service/tienda-service.service.ts
+++ Frontend/GameMate/src/app/service/tienda-service.service.ts
@@ -2,16 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Tienda } from '../Tienda';
+import { Producto } from 'app/Producto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TiendaServiceService {
-  private url = 'http://localhost:8000/api/v1productos/';
+  private apiUrl = 'http://localhost:8000/api/v1productos/';
 
   constructor(private http:HttpClient) { }
 
   public getTienda(): Observable<Tienda[]> {
-    return this.http.get<Tienda[]>(this.url);
+    return this.http.get<Tienda[]>(this.apiUrl);
+  }
+
+  public addToCart(): Observable<Producto[]> {
+    return this.http.post<any>(this.apiUrl, Producto);
+  }
+
+  public makePayment(paymentInfo: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, paymentInfo);
   }
 }
