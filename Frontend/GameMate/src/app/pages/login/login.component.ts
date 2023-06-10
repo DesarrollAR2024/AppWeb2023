2e6c5f5f416cd7b9f08b1f1623117e4654e8c2ed
EduRePiN <96556245+EduRePiN@users.noreply.github.com>
2023-06-10 16:27:38 -0300

    implementación login y registro

--- Frontend/GameMate/src/app/pages/login/login.component.ts
+++ Frontend/GameMate/src/app/pages/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'app/service/auth.service';
+import { Usuario, UsuarioService } from 'app/service/usuario.service';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +10,14 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  usuario: Usuario = new Usuario();
   form: FormGroup;
+  [x: string]: any;
 
-  constructor(private formBuilder: FormBuilder){
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router){
     this.form= this.formBuilder.group({
       mail:['',[Validators.required, Validators.email]],
-      password:['',[Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[A-Z]).*$/)]]
+      password:['',[Validators.required, Validators.minLength(8)]]
     })
   }
   
@@ -23,9 +28,33 @@ export class LoginComponent implements OnInit {
   get Mail(){
     return this.form.get("mail");
   }
+
+  get PasswordValid()
+  {
+    return this.Password?.touched && !this.Password?.valid;
+  }
+
+  get MailValid()
+  {
+    return this.Mail?.touched && !this.Mail?.valid;
+  }   
+
+  ngOnInit(){
   
-  onEnviar(event: Event){
+  }
+  
+  onEnviar(event: Event, usuario: Usuario): void{
     event.preventDefault;
+    this.authService.login(this.usuario)
+    .subscribe(
+      data => {
+      console.log("DATA" + JSON.stringify(data));
+
+      this.router.navigate(['']);
+    },
+    error => {
+      this['error'] = error;
+    });
     if (this.form.valid){
       alert("Enviar al servidor...")
     } else {
@@ -33,6 +62,5 @@ export class LoginComponent implements OnInit {
     }
   }
 
-  ngOnInit() {}
   
 }
--- Frontend/GameMate/src/app/pages/registro/registro.component.ts
+++ Frontend/GameMate/src/app/pages/registro/registro.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UsuarioService, Usuario } from 'app/service/usuario.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -9,12 +11,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class RegistroComponent implements OnInit {
   registroForm: FormGroup;
+  usuario: Usuario = new Usuario();
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private usuarioService: UsuarioService, private router: Router) {
     this.registroForm = this.formBuilder.group({
-      name: ['', Validators.required],
-      surname: ['', Validators.required],
-      user: ['', Validators.required],
+      nombre: ['', Validators.required],
+      apellido: ['', Validators.required],
+      nickname: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['',[ Validators.required , Validators.minLength(8), Validators.pattern(/^(?=.*[A-Z]).*$/)]]
     });
@@ -25,11 +28,47 @@ export class RegistroComponent implements OnInit {
 
   }
 
-  submitRegistroForm() {
+  submitRegistroForm(event: Event, usuario: Usuario): void {
+    event.preventDefault;
+
     if (this.registroForm.valid) {
-      console.log(this.registroForm.value);
+      console.log("Enviando al servidor...");
+      console.log(usuario);
+      this.usuarioService.onCrearUsuario(usuario).subscribe(
+        data => {
+          console.log(data.id);
+          if (data.id>0)
+          {
+            alert("El registro ha sido creado satisfactoriamente. A continuación, por favor Inicie Sesión");
+            this.router.navigate(['/login'])
+          }
+        }
+      )
     } else {
+      this.registroForm.markAllAsTouched();
       console.log('El formulario es inválido');
     }
   }
+
+  get Password1(){
+    return this.registroForm.get("passwword");
+  }
+
+  get Mail(){
+    return this.registroForm.get("email");
+  }
+
+  get Nombre(){
+    return this.registroForm.get("nombre");
+  }
+
+  get Apellido(){
+    return this.registroForm.get("apellido");
+  }
+
+  get Nickname(){
+    return this.registroForm.get("nickname");
+  }
+
+
 }
