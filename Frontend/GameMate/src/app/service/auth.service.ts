d58256214c598b5699fb0b49eef602588477040c
EduRePiN <96556245+EduRePiN@users.noreply.github.com>
2023-06-12 17:27:46 -0300

    login corregido

--- Frontend/GameMate/src/app/service/auth.service.ts
+++ Frontend/GameMate/src/app/service/auth.service.ts
@@ -1,21 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { LoginUsuario } from 'app/model/Login';
-import { HttpClient } from '@angular/common/http';
+import { Jwt } from 'app/model/Jwt';
+import { LoginUsuario } from 'app/model/Login-usuario';
+import { NuevoUsuario } from 'app/model/Nuevo-usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  url="http://localhost:8000/auth/login/";
+  url="http://localhost:8000/api/auth/login/";
 
   constructor(private http:HttpClient) {}
 
-  public nuevo(nuevoUsuario: LoginUsuario): Observable<any>{
+  public nuevo(nuevoUsuario: NuevoUsuario): Observable<any>{
     return this.http.post<any>(this.url, nuevoUsuario);
   }
 
-   public login(loginUsuario: LoginUsuario): Observable<any>{
+   public login(loginUsuario: LoginUsuario): Observable<Jwt>{
     return this.http.post<any>(this.url, loginUsuario)
    }
 }
